895ee506cf50c90be508d74ba1b1cf364a669dda
Archundia108 <gabrielaa.ti19@utsjr.edu.mx>
2022-04-05 13:11:21 -0500

    hosting integrado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDx661r3MSsB7HuhZBMR-PDtUDWdSEFjbY",
+    authDomain: "deliveryesquietes.firebaseapp.com",
+    projectId: "deliveryesquietes",
+    storageBucket: "deliveryesquietes.appspot.com",
+    messagingSenderId: "197457860829",
+    appId: "1:197457860829:web:7d30f4b2964336467b6334"
+  }
 };
